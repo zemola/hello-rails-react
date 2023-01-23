1ee5c9307a9f8347ea05b3df9fc3c13b202ccd65
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-01-23 09:02:47 +0100

    Update Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root to: 'root#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/greeting', to: 'messages#index'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
 end
